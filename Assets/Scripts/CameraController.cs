c52aad29bc483ad2036cc7a6f424ec02a9f7d0ee
BuildTools <unconfigured@null.spigotmc.org>
2018-10-16 23:15:29 +0900

    ・CameraControllerの追加

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform CameraTransform;
+    private float y, z;
+
+    void Start()
+    {
+        CameraTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        y = CameraTransform.position.y;
+        z = CameraTransform.position.z;
+        CameraTransform.position = new Vector3(0.0f, y, z);
+    }
+}
