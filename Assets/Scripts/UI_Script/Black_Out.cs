d55acafcdd8a97c7570ecc1d5afb2ef9ea96ee02
maekawatakuto <ga000000385507@outlook.jp>
2018-10-19 17:06:30 +0900

    ブラックアウト

---
+++ Assets/Scripts/UI_Script/Black_Out.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Black_Out : MonoBehaviour {
+    [SerializeField]
+    Game_Start game_start;
+    Image image;
+    public float a;
+    public int fade_timer;
+	// Use this for initialization
+	void Start () {
+        a = 0;
+         image = GetComponent<Image>();
+        image.color = new Color(0, 0, 0,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (game_start.fade_out)
+        {
+            a += 0.1f;
+            fade_timer +=(int)Time.deltaTime;
+            Debug.Log(a);
+        }
+        if (a>=1f){
+            game_start.fade_out = false;
+            SceneManager.LoadScene("diffeculty");
+            }
+        
+        image.color = new Color(0, 0, 0, a);
+    }
+}
---
+++ Assets/Scripts/UI_Script/Game_Start.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Game_Start : MonoBehaviour {
+    public bool fade_out = false;
+    public void change_scene()
+    {
+        fade_out = true;          
+    }
+}
